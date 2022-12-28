4a8df1c5a3cfd41fc1e05d97b7e6b3f642d79625
NacreousDawn596 <kamalafriadwac@gmail.com>
2022-12-28 21:35:58 +0100

    test

--- server.py
+++ server.py
@@ -6,4 +6,8 @@ app = flask.Flask(__name__, template_folder="./")
 def main():
     return flask.render_template("index.html")
 
+@app.route("/<file>")
+def upload(file):
+    return flask.send_file(file)
+
 app.run(host='0.0.0.0', port=8080)
